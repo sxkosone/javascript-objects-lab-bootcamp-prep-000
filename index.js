d89b5d0148c9d2f9ed73b8fe8e2d42513604a3f7
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-09 21:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
